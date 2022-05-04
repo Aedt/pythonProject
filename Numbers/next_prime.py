f22d092be5bb1545bca7114e81e0ae5b1869f8d9
Aedt <rogowski162@gmail.com>
2022-05-04 16:09:36 +0200

    karan's project list

--- Numbers/next_prime.py
+++ Numbers/next_prime.py
@@ -1,3 +1,31 @@
-"""Next Prime Number - Have the program find prime numbers until the user chooses to stop asking for the next one.
-
+"""next prime number - have the program find prime numbers until the user
+chooses to stop asking for the next one.
 """
+
+
+def next_prime(x):
+
+    for i in range(x + 1, 2 * x - 1):
+        k = 0
+        for j in range(1, i + 1):
+            if i % j == 0:
+                k += 1
+
+        if k == 2:
+            return i
+            break
+
+
+def main():
+    while True:
+        entry = input('->> ')
+        if entry == 'q':
+            break
+        if not entry.isdigit():
+            print('Not a digit, try again, \'q\' to quit.')
+            continue
+        print(next_prime(int(entry)))
+
+
+if __name__ == '__main__':
+    main()
